fc5c25491711bf8e28595b82f055ac374729d3a0
Claus Guttesen <kometen@gmail.com>
2014-07-01 11:33:51 +0200

    Changed autocomplete to jquery

--- client/views/races/race_add_participant.js
+++ client/views/races/race_add_participant.js
@@ -1,4 +1,9 @@
 Template.raceAddParticipant.events({
+	'keypress raceAddParticipant#name': function (e) {
+		if (e.which == 13) {
+			console.log('enter key pressed');
+		}
+	},
 	'submit form': function (e) {
 		e.preventDefault();
 
@@ -8,7 +13,9 @@ Template.raceAddParticipant.events({
 			name: $(e.target).find('[name=name]').val(),
 			year: $(e.target).find('[name=year]').val(),
 			club: $(e.target).find('[name=club]').val(),
-			id: $(e.target).find('[name=_id]').val()
+			id: $(e.target).find('[name=id]').val(),
+			submitted: new Date().getTime(),
+			raceId: raceId
 		}
 
 /*		Meteor.call('postParticipantToRace', participant, function (error, id) {
@@ -18,7 +25,7 @@ Template.raceAddParticipant.events({
 			Router.go('participantPage', {_id: id});
 		});*/
 
-		console.log('raceId: ' + raceId + ', name: ' + participant.name + ', club: ' + Session.get('currentUserClub') + ', id: ' + Session.get('currentUserId'));
+		console.log('raceId: ' + raceId + ', name: ' + participant.name + ', club: ' + participant.club + ', id: ' + participant.id);
 	}
 
 /*	'click .delete': function (e) {
@@ -32,21 +39,69 @@ Template.raceAddParticipant.events({
 	}*/
 });
 
-Template.raceAddParticipant.settings = function () {
-	return {
-		position: 'bottom',
-		limit: 10,
-		rules: [
-			{
-				collection: Participants,
-				field: 'name',
-				matchAll: true,
-				callback: function (doc) {
-					Session.set('currentUserClub', doc.club);
-					Session.set('currentUserId', doc._id);
-				},
-				template: Template.acParticipants
-			}
-		]
-	}
+Template.raceAddParticipant.rendered = function () {
+	document.getElementById('name').focus();
+
+	$("#name").autocomplete({
+		minLength: 0,
+//		source: participantsData,
+		source: function (request, response) {
+			response(participantsData)
+		},
+		focus: function (event, ui) {
+			$("#name").val(ui.item.value)
+			return false;
+		},
+		select: function (event, ui) {
+			$("#name").val(ui.item.value);
+			$("#club").val(ui.item.club);
+			$("#id").val(ui.item._id);
+			return false;
+		}
+	});
 };
+
+var participantsData = [
+	{
+		value: 'Claus Guttesen',
+		club: 'BBIL',
+		_id: 'abcd1234'
+	},
+	{
+		value: 'Eline Clausdatter Kleppenes',
+		club: 'BBIL',
+		_id: 'efgh5678'
+	},
+	{
+		value: 'Malene Clausdatter Kleppenes',
+		club: 'BBIL',
+		_id: 'ijkl9012'
+	},
+	{
+		value: 'Vincent Claussøn Kleppenes',
+		club: 'BBIL',
+		_id: 'emno9012'
+	},
+	{
+		value: 'Anne-Merete Kleppenes',
+		club: 'BBIL',
+		_id: 'pqrs3456'
+	},
+	{
+		value: 'Anders Felde',
+		club: 'BBIL',
+		_id: 'tuvx7890'
+	}
+];
+
+Template.raceAddParticipant.helpers({
+	participants: participantsData
+});
+
+function extractLast( term ) {
+  return split( term ).pop();
+}
+
+function split( val ) {
+  return val.split( /,\s*/ );
+}
--- server/publications.js
+++ server/publications.js
@@ -10,6 +10,6 @@ Meteor.publish('races', function () {
 	return Races.find();
 });
 
-Meteor.publish('participantsInRace', function () {
-	return ParticipantsInRace.find();
+Meteor.publish('participantsInRace', function (raceId) {
+	return ParticipantsInRace.find({raceId: raceId}, {sort: {submitted: 1}});
 });
