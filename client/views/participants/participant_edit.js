de1f1e924a30e446c726f9f5222ca52e6a28e0a8
Claus Guttesen <kometen@gmail.com>
2014-07-03 08:56:40 +0200

    Moved common code to new file
    And did some clean up.

--- client/views/participants/participant_edit.js
+++ client/views/participants/participant_edit.js
@@ -33,28 +33,6 @@ Template.participantEdit.events({
 Template.participantEdit.rendered = function () {
 	document.getElementById('name').focus();
 
-	$("#club").autocomplete({
-		minLength: 0,
-		source: function (request, response) {
-			var cData = Clubs.find({name: {$regex: new RegExp(request.term), $options: 'i'}}, {sort: {name: 1}});
-			var c = cData.fetch();
-			var suggestions = [];
-			console.log('request.term: ' + request.term + ', cData: ' + cData);
-			for (var i = 0; i < c.length; i++) {
-				console.log('c[' + i + '].name: ' + c[i].name + ', city: ' + c[i].city + ', id: ' + c[i]._id);
-				suggestions.push({value: c[i].name, club: c[i].city, _id: c[i]._id});
-			}
-			response(suggestions)
-		},
-		focus: function (event, ui) {
-			$("#club").val(ui.item.value)
-			return false;
-		},
-		select: function (event, ui) {
-			$("#club").val(ui.item.value);
-			$("#city").val(ui.item.club);
-			$("#id").val(ui.item._id);
-			return false;
-		}
-	});
+	acClub();
+
 };
