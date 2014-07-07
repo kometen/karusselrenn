bfa796895faf55089f860d1a20c8a3f184e001b0
Claus Guttesen <kometen@gmail.com>
2014-07-07 09:59:37 +0200

    Use momentjs to display starttime

--- client/views/races/race_page.js
+++ client/views/races/race_page.js
@@ -2,7 +2,7 @@ Template.racePage.participants = function () {
 	var objects = ParticipantsInRace.find({raceId: this._id}, {sort: {submitted: 1}}).map(function (doc, index, cursor) {
 		var i = _.extend(doc, {
 			index: (index + 1),
-			time: (index * doc.interval)
+			time: moment(doc.starttime, "HH:mm").add('seconds', doc.interval * index).format("HH:mm:ss")
 		});
 		return i;
 	});
