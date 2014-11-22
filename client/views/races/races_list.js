c29c9a21737442a708300d3986ff7f08df5f8ccb
Claus Guttesen <kometen@gmail.com>
2014-11-22 23:49:18 +0000

    Borrowed example from bootstrap3 which gave unexpected results.

--- client/views/races/races_list.js
+++ client/views/races/races_list.js
@@ -13,22 +13,33 @@ Template.racesList.events({
 
 		bootbox.dialog({
 			title: "Enter race details",
-			message:'<div class="row"> ' +
-				'<div class="span12"> ' +
-				'<form class="form-horizontal"> ' +
-				'<div class="form-group"> ' +
-				'<label class="span4 control-label" for="name">Name</label> ' +
-				'<div class="span4"> ' +
-				'<input id="name" name="name" type="text" placeholder="Racename" class="form-control" input-md"> ' +
-				'<span class="help-block">Here goes the racename</span> </div> ' +
-				'</div> ' +
-				'</form></div>',
+			message:'<form class="form-horizontal"> ' +
+					'<div class="control-group"> ' +
+						'<label class="control-label" for="name">Name</label> ' +
+						'<div class="controls"> ' +
+							'<input id="name" name="name" type="text" placeholder="Racename"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="control-group"> ' +
+						'<label class="control-label" for="location">Location</label> ' +
+						'<div class="controls"> ' +
+							'<input id="location" name="location" type="text" placeholder="Location"> ' +
+						'</div> ' +
+					'</div> ' +
+				'</form>',
 			buttons: {
 				success: {
 					label: "Save",
 					className: "btn-success",
 					callback: function () {
-						console.log("form dialog");
+						console.log("add race");
+					}
+				},
+				cancel: {
+					label: "Cancel",
+					className: "btn-default",
+					callback: function () {
+						console.log("cancel adding race");
 					}
 				}
 			}
