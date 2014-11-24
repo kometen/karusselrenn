89bfe747f33829b47c08214d7e44bb7e861bc88d
Claus Guttesen <kometen@gmail.com>
2014-11-24 21:07:35 +0100

    Add race using modal window.

--- client/views/races/races_list.js
+++ client/views/races/races_list.js
@@ -14,16 +14,34 @@ Template.racesList.events({
 		bootbox.dialog({
 			title: "Enter race details",
 			message:'<form class="form-horizontal"> ' +
-					'<div class="control-group"> ' +
+					'<div class="form-group"> ' +
 						'<label class="control-label" for="name">Name</label> ' +
 						'<div class="controls"> ' +
 							'<input id="name" name="name" type="text" placeholder="Racename"> ' +
 						'</div> ' +
 					'</div> ' +
-					'<div class="control-group"> ' +
+					'<div class="form-group"> ' +
 						'<label class="control-label" for="location">Location</label> ' +
 						'<div class="controls"> ' +
 							'<input id="location" name="location" type="text" placeholder="Location"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="date">Date</label> ' +
+						'<div class="controls"> ' +
+							'<input id="date" name="date" type="text" placeholder="Date"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="time">Time</label> ' +
+						'<div class="controls"> ' +
+							'<input id="time" name="time" type="text" placeholder="Time"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="interval">Interval</label> ' +
+						'<div class="controls"> ' +
+							'<input id="interval" name="interval" type="text" placeholder="Interval"> ' +
 						'</div> ' +
 					'</div> ' +
 				'</form>',
@@ -32,7 +50,20 @@ Template.racesList.events({
 					label: "Save",
 					className: "btn-success",
 					callback: function () {
-						console.log("add race");
+						var race = {
+							name: $('#name').val(),
+							location: $('#location').val(),
+							date: $('#date').val(),
+							time: $('#time').val(),
+							interval: $('#interval').val()
+						}
+						console.log("add race with name " + race.name + " at " + race.location);
+						Meteor.call('postRace', race, function (error, id) {
+							if (error) {
+								return alert(error.reason);
+							}
+							Router.go('racePage', {_id: id});
+						});
 					}
 				},
 				cancel: {
