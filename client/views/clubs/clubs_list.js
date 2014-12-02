06d17f2dd0a1cf1cc5c40097290adc8c50acc105
Claus Guttesen <kometen@gmail.com>
2014-12-02 22:21:13 +0100

    New club and participant in modal window.

--- client/views/clubs/clubs_list.js
+++ client/views/clubs/clubs_list.js
@@ -6,3 +6,72 @@ Template.clubsList.helpers({
 		return Meteor.userId();
 	}
 });
+
+Template.clubsList.events({
+
+	'click input.new': function (e) {
+		e.preventDefault();
+
+		// stackoverflow.com/questions/22009649/how-to-create-a-bootbox-prompt-with-a-bootstrap-datepicker-as-input
+
+		function BootboxContent(){
+            var frm_str = '<form class="form-horizontal"> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="name">Name</label> ' +
+						'<div class="controls"> ' +
+							'<input id="name" name="name" type="text" placeholder="Clubname"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="description">Description</label> ' +
+						'<div class="controls"> ' +
+							'<input id="description" name="description" type="text" placeholder="Description"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="city">City</label> ' +
+						'<div class="controls"> ' +
+							'<input id="city" name="city" type="text" placeholder="City"> ' +
+						'</div> ' +
+					'</div> ' +
+				'</form>';
+
+    	    var object = $('<div/>').html(frm_str).contents();
+
+        	return object
+	    }
+
+	    //Show the datepicker in the bootbox
+    	bootbox.dialog({
+        	message: BootboxContent,
+	        title: "Enter club details",
+    	    buttons: {
+				success: {
+					label: "Save",
+					className: "btn-success",
+					callback: function () {
+						var club = {
+							name: $('#name').val(),
+							description: $('#description').val(),
+							city: $('#city').val()
+						}
+						console.log("add club with name " + club.name + " at " + club.city);
+                        Meteor.call('postClub', club, function (error, id) {
+                            if (error) {
+                                return alert(error.reason);
+                            }
+                            Router.go('clubsList');
+                        });
+        			}
+			    },
+				cancel: {
+					label: "Cancel",
+					className: "btn-default",
+					callback: function () {
+						console.log("cancel adding club");
+					}
+				}
+			}
+		});
+    }
+});
--- client/views/participants/participants_list.js
+++ client/views/participants/participants_list.js
@@ -6,3 +6,83 @@ Template.participantsList.helpers({
 		return Meteor.userId();
 	}
 });
+
+Template.participantsList.events({
+
+	'click input.new': function (e) {
+		e.preventDefault();
+
+		// stackoverflow.com/questions/22009649/how-to-create-a-bootbox-prompt-with-a-bootstrap-datepicker-as-input
+
+		function BootboxContent(){
+            var frm_str = '<form class="form-horizontal"> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="name">Name</label> ' +
+						'<div class="controls"> ' +
+							'<input id="name" name="name" type="text" placeholder="Participants name"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="year">Year</label> ' +
+						'<div class="controls"> ' +
+							'<input id="year" name="year" type="text" placeholder="Year born"> ' +
+						'</div> ' +
+					'</div> ' +
+					'<div class="form-group"> ' +
+						'<label class="control-label" for="club">Club</label> ' +
+						'<div class="controls"> ' +
+							'<input id="raceclub" name="club" type="text" placeholder="Club"> ' +
+						'</div> ' +
+					'</div> ' +
+				'</form>';
+
+    	    var object = $('<div/>').html(frm_str).contents();
+
+        	object.find('.date').datepicker({
+            	format: 'dd-mm-yyyy',
+	            autoclose: true}).on('changeDate', function (ev) {
+    	           $(this).blur();
+        	       $(this).datepicker('hide');
+        	});
+
+        	return object
+	    }
+
+	    //Show the datepicker in the bootbox
+    	bootbox.dialog({
+        	message: BootboxContent,
+	        title: "Enter participant details",
+    	    buttons: {
+				success: {
+					label: "Save",
+					className: "btn-success",
+					callback: function () {
+						var participant = {
+							name: $('#name').val(),
+							year: $('#year').val(),
+							club: $('#club').val()
+						}
+						console.log("add participant with name " + participant.name + " at club " + participant.club);
+                        Meteor.call('postParticipant', participant, function (error, id) {
+                            if (error) {
+                                return alert(error.reason);
+                            }
+//                            Router.go('racePage', {_id: id});
+                        });
+        			}
+			    },
+				cancel: {
+					label: "Cancel",
+					className: "btn-default",
+					callback: function () {
+						console.log("cancel adding participant");
+					}
+				}
+			}
+		});
+    }
+});
+
+Template.participantsList.rendered = function () {
+	acClub();
+};
